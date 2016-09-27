c2740fb5d19f38abe6191e494bf383ef10a62282
Ian Curry <ian@dasein.me>
2016-09-27 14:17:21 -0700

    run

--- controllers/message.js
+++ controllers/message.js
@@ -36,8 +36,7 @@ exports.showReceiveMessage = function(request, response) {
 
 // Handle a POST request from Twilio for an incoming message
 exports.receiveMessageWebhook = function(request, response) {
-  console.log(request);
-  response.sendStatus(200);
+  console.log("got the message");
 };
 
 // Update the configured Twilio number for this demo to send all incoming
