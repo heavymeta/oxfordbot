c56a376b4e33fa76f914a56860704096d28e1506
Ian Curry <ian@dasein.me>
2016-09-28 14:17:09 -0700

    WORKING: comments

--- controllers/message.js
+++ controllers/message.js
@@ -24,25 +24,25 @@ var conn = mongoose.connection;
 // Render a form that will allow the user to send a text (or picture) message
 // to a phone number they entered.
 exports.showSendMessage = function(request, response) {
-    response.render('sendMessage', {
-        title: 'Sending Messages with Twilio'
-    });
+  response.render('sendMessage', {
+    title: 'Sending Messages with Twilio'
+  });
 };
 
 // Handle a form POST to send a message to a given number
 exports.sendMessage = function(request, response) {
   client.messages.create({
-   body: 'Hello from Node',
-   to: config.myNumber,  // Text this number
-   from: config.twilioNumber, // From a valid Twilio number
-   mediaUrl: 'https://demo.twilio.com/owl.png'
-}, function(err, message) {
-   if(err) {
-       console.error(err.message);
-   } else {
-     console.log("success");
-   }
-});
+    body: 'Hello from Node',
+    to: config.myNumber,  // Text this number
+    from: config.twilioNumber, // From a valid Twilio number
+    mediaUrl: 'https://demo.twilio.com/owl.png'
+  }, function(err, message) {
+    if(err) {
+      console.error(err.message);
+    } else {
+      console.log("success");
+    }
+  });
 };
 
 // Show a page displaying text/picture messages that have been sent to this
@@ -56,7 +56,7 @@ exports.receiveMessageWebhook = function(request, response) {
   var parsedTime = chrono.parseDate(request.body.Body);
   var parsedTimeLocal = moment(parsedTime).format(' dddd MMM DD, h:mm a ');
 
-// Save the remimder to the database
+  // Save the remimder to the database
 
   var myReminder = new Reminder()
   myReminder.from = request.body.From;
@@ -64,25 +64,25 @@ exports.receiveMessageWebhook = function(request, response) {
   myReminder.when = moment(chrono.parseDate(request.body.Body)).format();
   myReminder.fired = false;
   myReminder.save(
-  function(err){
-    //console.error("Error while saving");
-  }
+    function(err){
+      //console.error("Error while saving");
+    }
   );
 
   response.send("I got it");
 
   client.messages.create({
-   body: 'Got it. I\'m going to remind you on ' + parsedTimeLocal,
-   to: config.myNumber,  // Text this number
-   from: config.twilioNumber // From a valid Twilio number
-   //mediaUrl: 'https://demo.twilio.com/owl.png'
-}, function(err, message) {
-   if(err) {
-       console.error(err.message);
-   } else {
-     console.log("Message sent");
-   }
-});
+    body: 'Got it. I\'m going to remind you on ' + parsedTimeLocal,
+    to: config.myNumber,
+    from: config.twilioNumber
+    //mediaUrl: 'https://demo.twilio.com/owl.png'
+  }, function(err, message) {
+    if(err) {
+      console.error(err.message);
+    } else {
+      console.log("Message sent");
+    }
+  });
 
 };
 
@@ -90,30 +90,32 @@ function findReminders() {
   console.log(moment(new Date()).format());
   var foundReminders = Reminder.find({
     fired: false,
-    when: { $gt: moment().utcOffset('-0700') }
+    when: { $lt: moment().utcOffset('-0700').format() }
   })
   return foundReminders;
 }
 
+// Send reminder messages
 function sendReminders(reminder) {
   client.messages.create({
-   body: reminder.item,
-   to: reminder.from,  // Text this number
-   from: config.twilioNumber // From a valid Twilio number
+    body: reminder.item,
+    to: reminder.from,
+    from: config.twilioNumber
   }, function(err, message) {
-   if(err) {
-       console.error(err.message);
-   } else {
-     console.log("Successfully reminded: " + reminder.item);
-     markSent(reminder);
-   }
+    if(err) {
+      console.error(err.message);
+    } else {
+      console.log("Successfully reminded: " + reminder.item);
+      markSent(reminder);
+    }
   });
 }
 
+// Mark reminders as sent once they're fired.
 function markSent(reminder) {
   Reminder.update({_id: reminder._id },{"$set":{fired:true}},{ multi: false }, function(err, affected, resp) {
-   console.log(affected);
-});
+    console.log(affected);
+  });
   console.log("Marked as sent: " + reminder._id);
 }
 
@@ -121,12 +123,12 @@ function markSent(reminder) {
 exports.fireReminders = function(request, response) {
   var query = findReminders();
   query.exec(function(err,reminders){
-   if(err)
-      return console.log(err);
-   reminders.forEach(function(reminder){
+    if(err)
+    return console.log(err);
+    reminders.forEach(function(reminder){
       sendReminders(reminder);
       console.log("Found: " + reminder.item);
-   });
-});
+    });
+  });
   //response.sendMessage(200);
 };
