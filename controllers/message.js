e592776591b40d95774b3e7b003230de31437395
heavymeta <ian@heavy-meta.com>
2016-11-09 21:54:40 -0800

    d

--- controllers/message.js
+++ controllers/message.js
@@ -79,6 +79,16 @@ function photoParse(img) {
     });
 }
 
+function traverse(o,func) {
+    for (var i in o) {
+        func.apply(this,[i,o[i]]);
+        if (o[i] !== null && typeof(o[i])=="object") {
+            //going on step down in the object tree!!
+            traverse(o[i],func);
+        }
+    }
+}
+
 function process(key,value) {
     console.log(key + " : "+value);
     if (key == "text") {
