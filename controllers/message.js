b69510ba399aa441a0be4cdec7cfc0f0ce9fd6ae
heavymeta <ian@heavy-meta.com>
2016-11-09 21:51:12 -0800

    image

--- controllers/message.js
+++ controllers/message.js
@@ -60,14 +60,14 @@ exports.sendMessage = function(request, response) {
 // Show a page displaying text/picture messages that have been sent to this
 // web application, which we have stored in the database
 
-function photoParse(response) {
+function photoParse(img) {
 
   request({
       headers: {
         'Ocp-Apim-Subscription-Key': '708e44803f7d4b86b5c988d9c7816f3a'
       },
       uri: 'https://api.projectoxford.ai/vision/v1.0/ocr?language=unk&detectOrientation=true',
-      body: "{'url':'https://api.twilio.com/2010-04-01/Accounts/AC9da8e02953dc14e2cf46f01c513f5592/Messages/MM79d849214fc1d4f0a59ba93f13fd6e21/Media/ME6054833a65fa309940aa464fb47ecac3'}",
+      body: "{'url':"+img+"}",
       method: 'POST'
     }, function (err, res, body) {
 
@@ -97,6 +97,7 @@ exports.receiveMessageWebhook = function(request, response) {
 
   if (image) {
     console.log("got an image");
+    photoParse(image);
   }
 
 response.sendStatus(200);
