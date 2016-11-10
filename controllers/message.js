1b6bd00e50fd2e9d50ae77b6076fd309f82af5c6
Ian Curry <ian@dasein.me>
2016-11-10 10:01:01 -0800

    s

--- controllers/message.js
+++ controllers/message.js
@@ -174,7 +174,7 @@ function process(key,value) {
 
 // Handle a POST request from Twilio
 exports.receiveMessageWebhook = function(request, response) {
-
+response.sendStatus(200);
 
   words = ""
   var message = request.body.Body;
@@ -191,7 +191,7 @@ exports.receiveMessageWebhook = function(request, response) {
   } else {
     sendDateResponse(parsedTimeLocal);
   }
-  response.sendStatus(200);
+
 };
 
 function sendDateResponse(time) {
