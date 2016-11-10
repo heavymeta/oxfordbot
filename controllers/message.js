842f63896b983311ea5fde187e6b37abdc242b07
heavymeta <ian@heavy-meta.com>
2016-11-10 00:45:32 -0800

    ss

--- controllers/message.js
+++ controllers/message.js
@@ -175,6 +175,7 @@ function process(key,value) {
 // Handle a POST request from Twilio
 exports.receiveMessageWebhook = function(request, response) {
 
+  response.sendStatus(200);
   words = ""
   var message = request.body.Body;
   var image = request.body.MediaUrl0;
@@ -190,9 +191,6 @@ exports.receiveMessageWebhook = function(request, response) {
   } else {
     sendDateResponse(parsedTimeLocal);
   }
-
-
-response.sendStatus(200);
 };
 
 function sendDateResponse(time) {
